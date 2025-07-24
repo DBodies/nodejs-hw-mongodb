10cf6b1a4fc52700973b4a35e7742dba9bf9feed
Denis <159953887+DBodies@users.noreply.github.com>
2025-07-24 08:18:04 +0300

    added status 200 in rout /contacts

--- src/server.js
+++ src/server.js
@@ -25,6 +25,7 @@ export const setupServer = () => {
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
             message: "Successfully found contacts!",
             data: contacts
         });
