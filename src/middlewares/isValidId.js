d4fb0d1e5d6480d7ae8062db9974f763c28d6d18
Denis <159953887+DBodies@users.noreply.github.com>
2025-08-04 14:29:31 +0300

    fixed HW

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -5,7 +5,7 @@ import createHttpError from "http-errors";
 export const isValidId = (req, res, next) => {
     const { contactsId } = req.params;
     if (!isValidObjectId(contactsId)) {
-       return next(createHttpError(400, 'Bad request'));
+       return next(createHttpError(500, 'Bad request'));
     }
     next();
 };
